9ae845635e4287d911ea287e2ebfd9671cc2cb27
StackMasterV <168885298+groupfswd@users.noreply.github.com>
2024-05-06 22:08:07 +0700

    Ikram (#1)
    * rebase development
    
    * authentication and authorization, cms category
    
    * delete migration
    
    ---------
    
    Co-authored-by: Taufik Hidayat <hidytt.taufik@gmail.com>
    Co-authored-by: Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,26 @@
-const authService = require('../services/AuthService');
+const authService = require("../services/AuthService");
 
-const register = (req, res, next) => {}
+const register = async (req, res, next) => {
+  try {
+    const newUser = req.body;
+    const user = await authService.register(newUser);
+    res.status(201).json({ message: "Register Success" });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
-const login = (req, res, next) => {}
-
-
+const login = async (req, res, next) => {
+  try {
+    const userLogin = req.body;
+    await authService.login(userLogin);
+    res.status(200).json({ message: "Login Success" });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+};
--- services/AuthService.js
+++ services/AuthService.js
@@ -1,5 +1,61 @@
-const login = (params) =>{}
+const { hashPassword, validPassword } = require("../lib/bcrypt");
+const { PrismaClient } = require("@prisma/client");
+const { generateToken } = require("../lib/jwt");
+const prisma = new PrismaClient();
 
-const register = (params) =>{}
+const register = async (params) => {
+  try {
+    const { fullname, email, password, phone_number } = params;
+    const hashedPassword = hashPassword(password);
+    const user = await prisma.users.create({
+      data: {
+        fullname,
+        email,
+        password: hashedPassword,
+        phone_number,
+      },
+    });
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-module.exports = {login, register}
+const login = async (params) => {
+  try {
+    const { email, password } = params;
+    const user = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw {
+        name: "InvalidCredentials",
+        message: "Email or password is wrong",
+      };
+    }
+
+    const isValidPassword = validPassword(password, user.password);
+    if (!isValidPassword) {
+      throw {
+        name: "InvalidCredentials",
+        message: "Email or password is wrong",
+      };
+    }
+
+    const token = generateToken({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    console.log(token);
+
+    return user;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { login, register };
