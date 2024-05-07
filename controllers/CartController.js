7df51dceec6cabb7689b83ea34407d5c557c71bc
0kasama <okaajyad@gmail.com>
2024-05-07 15:51:09 +0700

    chore(cart): update cartservice and cartcontroller

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,10 +1,27 @@
-const cartService = require('../services/CartService');
+const cartService = require("../services/CartService");
 
-const findOne = async (req, res, next) => {}
+const findOne = async (req, res, next) => {
+  try {
+    const cart = await cartService.findOne({ id: req.params.id });
+    res.json(cart);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const update = async (req, res, next) => {}
+const update = async (req, res, next) => {
+  try {
+    const cart = await cartService.update({
+      id: req.params.id,
+      quantity: req.body.quantity,
+    });
+    res.json(cart);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    findOne,
-    update
-}
+  findOne,
+  update,
+};
--- services/CartService.js
+++ services/CartService.js
@@ -1,11 +1,27 @@
 const prisma = require("../lib/prisma");
 
-const findOne = async (params) => {}
+const findOne = async (params) => {
+  const cart = await prisma.cart.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  return cart;
+};
 
-const update = async (params) => {}
-
+const update = async (params) => {
+  const cart = await prisma.cart.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      quantity: params.quantity,
+    },
+  });
+  return cart;
+};
 
 module.exports = {
-    findOne,
-    update
-}
+  findOne,
+  update,
+};
