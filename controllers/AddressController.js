fba0f5c62452ff9ecf1451e79df13d896696f688
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-05-08 20:40:59 +0700

    create controller and service address

--- controllers/AddressController.js
+++ controllers/AddressController.js
@@ -1,14 +1,59 @@
 const addressService = require('../services/AddressService');
 
-const findAll = async (req, res, next) => {}
+const findAll = async (req, res, next) => {
+    try {
+        const data = await addressService.findAll(req.loggedUser.id)
 
-const findOne = async (req, res, next) => {}
+        res.status(200).json({ message: 'Get All Adresseses Users Logged Success', data })
+    } catch (err) {
+        next(err)
+    }
+}
 
-const create = async (req, res, next) => {}
+const findOne = async (req, res, next) => {
+    try {
+        const data = await addressService.findOne(req.params.id)
 
-const update = async (req, res, next) => {}
+        res.status(200).json({ message: 'Get Address By Id Success', data})
+    } catch (error) {
+        next(error)
+    }
+}
 
-const destroy = async (req, res, next) => {}
+const create = async (req, res, next) => {
+    try {
+        const data = await addressService.create(req.body);
+
+        res.status(201).json({ message: 'Successfully create address', data})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const update = async (req, res, next) => {
+    try {
+        console.log(req.params.id);
+        const data = { 
+            id: req.params.id,
+            data: req.body
+        }
+        const address = await addressService.update(data)
+        
+        res.status(201).json({ message: 'Updated Success', address})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const destroy = async (req, res, next) => {
+    try {
+        const data = await addressService.destroy(req.params.id)
+
+        res.status(200).json({ message: 'Successfully Deleted Address', data})
+    } catch (error) {
+        next(error)
+    }
+}
 
 
 module.exports = {
--- services/AddressService.js
+++ services/AddressService.js
@@ -1,14 +1,111 @@
 const prisma = require("../lib/prisma");
 
-const findAll = async (params) => {}
+const findAll = async (params) => {
+    const user_id = +params;
+    const addresses = await prisma.addresses.findFirst({
+        where: {
+            user_id: user_id
+        }
+    })
 
-const findOne = async (params) => {}
+    if(!addresses){
+        throw {
+            name: "ErrorNotFound",
+            message: "Data Address User Not Found"
+        }
+    }
 
-const create = async (params) => {}
+    return addresses;
+}
 
-const update = async (params) => {}
+const findOne = async (params) => {
+    const id = +params
+    const address = await prisma.addresses.findUnique({
+        where: {
+            id: id
+        }
+    })
 
-const destroy = async (params) => {}
+    if(!address){
+        throw {
+            name: "ErrorNotFound",
+            message: "Data Not Found"
+        }
+    }
+
+    return address;
+}
+
+const create = async (params) => {
+    const address = await prisma.addresses.create({
+        data: {
+            user_id: params.user_id,
+            city_id: params.city_id,
+            street_address: params.street_address,
+            province: params.province,
+            postal_code: params.postal_code
+        }
+    });
+
+    if (!address) {
+        throw {
+            name: "ErrorNotFound",
+            message: "Data Not Found"
+        }
+    }
+    return address;
+}
+
+const update = async (params) => {
+    const id = await prisma.addresses.findUnique({
+        where: {
+            id: +params.id
+        }
+    });
+
+    if (!id) {
+        throw {
+            name: "ErrorNotFound",
+            message: "Data Not Found"
+        }
+    }
+
+    const address = await prisma.addresses.update({
+        where: { id: +params.id },
+        data: {
+            user_id: params.data.user_id,
+            city_id: params.data.city_id,
+            street_address: params.data.street_address,
+            province: params.data.province,
+            postal_code: params.data.postal_code
+        }
+    });
+
+    return address;
+}
+
+const destroy = async (params) => {
+    const data = await prisma.addresses.findUnique({
+        where: {
+            id: +params
+        }
+    });
+
+    if (!data) {
+        throw {
+            name: "ErrorNotFound",
+            message: "Data Not Found"
+        }
+    }
+    
+    const address = await prisma.addresses.delete({
+        where: {
+            id: parseInt(params)
+        }
+    });
+
+    return address;
+}
 
 
 module.exports = { findAll, findOne, create, update, destroy }
