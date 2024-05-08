50ce49bf11616ba5b580ac60ab87a9f87e7fb2df
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-05-08 10:06:02 +0800

    create service and controllers for cities

--- controllers/CityController.js
+++ controllers/CityController.js
@@ -1,10 +1,25 @@
-const cityService = require('../services/CityService');
+const cityService = require("../services/CityService");
 
-const findAll = async (req, res, next) => {}
+const findAll = async (req, res, next) => {
+  try {
+    const data = await cityService.findAll();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
 
-const findOne = async (req, res, next) => {}
+const findOne = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = await cityService.findOne(parseInt(id));
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    findAll,
-    findOne
-}
+  findAll,
+  findOne,
+};
