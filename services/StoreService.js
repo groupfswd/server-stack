bb8720ff6b5e0067a06bea758ad865998cec7ebf
ibanganteng <mochammad.ridwan931@gmail.com>
2024-05-09 02:43:25 +0700

    menambahkan service

--- services/StoreService.js
+++ services/StoreService.js
@@ -1,20 +1,75 @@
 const prisma = require("../lib/prisma");
 
-const findAll = async (params) => {}
+const findAll = async (params) => {
+  const data = await prisma.stores.findMany();
+  return data;
+};
 
-const findOne = async (params) => {}
+const findOne = async (params) => {
+  const data = await prisma.stores.findUnique({
+    where: {
+      id: params,
+    },
+  });
+  if (!data) {
+    throw Error;
+  }
+  return data;
+};
 
-const create = async (params) => {}
+const create = async (params) => {
+  const data = await prisma.stores.create({
+    data: {
+      city_id: params.city_id,
+      name: params.name,
+      bank_name: params.bank_name,
+      street_address: params.street_address,
+      province: params.province,
+      postal_code: params.postal_code,
+    },
+  });
+  if (!data) {
+    throw {
+      message: "error data not found",
+    };
+  }
+  return data;
+};
 
-const update = async (params) => {}
+const update = async (id, params) => {
+  await findOne(id);
+  const data = await prisma.stores.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      city_id: params.city_id,
+      name: params.name,
+      bank_name: params.bank_name,
+      street_address: params.street_address,
+      province: params.province,
+      postal_code: params.postal_code,
+    },
+  });
+  return data;
+};
 
-const destroy = async (params) => {}
-
+const destroy = async (params) => {
+  const data = await prisma.stores.destroy({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!data) {
+    throw Error;
+  }
+  return data;
+};
 
 module.exports = {
-    findAll,
-    findOne,
-    create,
-    update,
-    destroy
-}
+  findAll,
+  findOne,
+  create,
+  update,
+  destroy,
+};
