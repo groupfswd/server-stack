080956630e4b46ac44eb505e1c6347dc315d126c
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-05-08 15:23:38 +0800

    create findAll user order with loggedUser id

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,17 +1,24 @@
-const orderService = require('../services/OrderService');
+const orderService = require("../services/OrderService");
 
-const findAll = async (req, res, next) => {}
+const findAll = async (req, res, next) => {
+  try {
+    const id = req.loggedUser.id;
+    const data = await orderService.findAll(parseInt(id));
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
 
-const findOne = async (req, res, next) => {}
+const findOne = async (req, res, next) => {};
 
-const create = async (req, res, next) => {}
+const create = async (req, res, next) => {};
 
-const update = async (req, res, next) => {}
-
+const update = async (req, res, next) => {};
 
 module.exports = {
-    findAll,
-    findOne,
-    create,
-    update
-}
+  findAll,
+  findOne,
+  create,
+  update,
+};
--- services/OrderService.js
+++ services/OrderService.js
@@ -1,17 +1,27 @@
 const prisma = require("../lib/prisma");
 
-const findAll = async (params) => {}
+const findAll = async (params) => {
+  try {
+    const data = await prisma.orders.findMany({
+      where: {
+        user_id: params,
+      },
+    });
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
 
-const findOne = async (params) => {}
+const findOne = async (params) => {};
 
-const create = async (params) => {}
+const create = async (params) => {};
 
-const update = async (params) => {}
-
+const update = async (params) => {};
 
 module.exports = {
-    findAll,
-    findOne,
-    create,
-    update
-}
+  findAll,
+  findOne,
+  create,
+  update,
+};
