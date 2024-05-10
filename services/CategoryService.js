498b5a3024ba275dbbb2a17747531b331682d498
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-05-11 02:38:05 +0700

    Update Category On Delete Method

--- services/CategoryService.js
+++ services/CategoryService.js
@@ -6,40 +6,53 @@ const findAll = async () => {
 };
 
 const findOne = async (params) => {
-    const data = await prisma.categories.findUnique({
-      where: {
-        id: params,
-      },
-    });
+  const data = await prisma.categories.findUnique({
+    where: {
+      id: params,
+    },
+  });
 
-    if (!data) throw new Error("Data Not Found");
-    return data;
+  if (!data) throw {
+    name: "ErrorNotFound",
+    message: "Data Category Not Found"
+  };
+  return data;
 };
 
 const create = async (params) => {
-    const data = await prisma.categories.create({
-      data: {
-        name: params.name,
-      },
-    });
-    return data;
+  const data = await prisma.categories.create({
+    data: {
+      name: params.name,
+    },
+  });
+  return data;
 };
 
 const update = async (id, params) => {
-  const { name } = params;
+  const {
+    name
+  } = params;
   await findOne(id);
-    const data = await prisma.categories.update({
-      where: {
-        id: id,
-      },
-      data: {
-        name: name,
-      },
-    });
-    return data;
+  const data = await prisma.categories.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: name,
+    },
+  });
+  return data;
 };
 
-const destroy = async (params) => {};
+const destroy = async (params) => {
+  await findOne(+params);
+  const data = await prisma.categories.delete({
+    where: {
+      id: +params
+    }
+  })
+  return data;
+};
 
 module.exports = {
   findAll,
