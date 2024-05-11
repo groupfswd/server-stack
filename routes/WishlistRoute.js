c2fca35a651b0c1875b98b2cb074d631d57392bb
amufid <91891129+amufid@users.noreply.github.com>
2024-05-11 17:33:59 +0700

    update feature/wishlists

--- routes/WishlistRoute.js
+++ routes/WishlistRoute.js
@@ -3,7 +3,7 @@ const wishlistController = require("../controllers/WishlistController")
 
 router.get("/", wishlistController.findAll)
 router.get("/:id", wishlistController.findOne)
-router.post("/", wishlistController.create)
+router.post("/:id", wishlistController.create)
 router.delete("/:id", wishlistController.destroy)
 
 module.exports = router
