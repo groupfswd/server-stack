0a8015cc121c5fb22309e609c1bc43dd7efaa3b5
Abdul Mufid <91891129+amufid@users.noreply.github.com>
2024-05-11 18:02:45 +0700

    update feature/wishlists (#12)

--- services/WishlistService.js
+++ services/WishlistService.js
@@ -1,13 +1,57 @@
 const prisma = require("../lib/prisma");
 
-const findAll = async (params) => {}
+const findAll = async (params) => {
+    const wishlists = await prisma.wishlists.findMany({
+        where: { user_id: params.id },
+        include: {
+            product: true
+        }
+    })
 
-const findOne = async (params) => {}
+    return wishlists;
+}
 
-const create = async (params) => {}
+const findOne = async (params) => {
+    const wishlist = await prisma.wishlists.findUnique({
+        where: { id: +params },
+        include: {
+            product: true
+        }
+    })
 
-const destroy = async (params) => {}
+    if (!wishlist) {
+        throw { name: 'ErrorNotFound' }
+    }
 
+    return wishlist;
+}
+
+const create = async (params) => {
+    const wishlist = await prisma.wishlists.create({
+        data: {
+            user_id: params.user.id,
+            product_id: +params.product
+        }
+    })
+
+    return wishlist;
+}
+
+const destroy = async (params) => {
+    const result = await prisma.wishlists.findUnique({
+        where: { id: +params }
+    })
+
+    if (!result) {
+        throw { name: 'ErrorNotFound' }
+    }
+
+    const wishlist = await prisma.wishlists.delete({
+        where: { id: result.id }
+    })
+
+    return wishlist;
+}
 
 module.exports = {
     findAll,
