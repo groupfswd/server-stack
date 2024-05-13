a5b9f34659d47e4b73b12d8dc39167073b41b37b
0kasama <141619471+0kasama@users.noreply.github.com>
2024-05-13 21:42:59 +0700

    chore(review): update reviewservice and reviewcontroller (#4)

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,20 +1,57 @@
-const reviewService = require('../services/ReviewService');
+const reviewService = require("../services/ReviewService");
 
-const findAll = async (req, res, next) => {}
+const findAll = async (req, res, next) => {
+  try {
+    const review = await reviewService.findAll();
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const findOne = async (req, res, next) => {}
+const findOne = async (req, res, next) => {
+  try {
+    const review = await reviewService.findOne(req.params);
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const create = async (req, res, next) => {}
+const create = async (req, res, next) => {
+  try {
+    const review = await reviewService.create(req.body);
+    res.status(201).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const update = async (req, res, next) => {}
+const update = async (req, res, next) => {
+  try {
+    const review = await reviewService.update({
+      id: req.params.id,
+      body: req.body,
+    });
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const destroy = async (req, res, next) => {}
-
+const destroy = async (req, res, next) => {
+  try {
+    const review = await reviewService.destroy(req.params);
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    findAll,
-    findOne,
-    create,
-    update,
-    destroy
-}
+  findAll,
+  findOne,
+  create,
+  update,
+  destroy,
+};
