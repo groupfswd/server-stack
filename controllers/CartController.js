5720d649263b1ea9defff86fed3d48946f6d2200
0kasama <okaajyad@gmail.com>
2024-05-18 22:32:09 +0700

    chore(cart): Add getShippingCost

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -23,7 +23,17 @@ const update = async (req, res, next) => {
   }
 };
 
+const getShippingCost = async (req, res, next) => {
+  try {
+    const shipping_cost = await cartService.getShippingCost(req.query);
+    return res.status(200).json(shipping_cost);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   findOne,
   update,
+  getShippingCost,
 };
--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const cartController = require("../controllers/CartController")
 
 router.get("/", cartController.findOne)
+router.get("/shipping_costs", cartController.getShippingCost)
 router.put("/", cartController.update)
 
 module.exports = router
