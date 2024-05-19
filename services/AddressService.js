ef2bc774dfec2ec571b83000dae46f5a5dadb85b
0kasama <okaajyad@gmail.com>
2024-05-20 02:32:17 +0700

    chore(address): add title param

--- services/AddressService.js
+++ services/AddressService.js
@@ -1,110 +1,111 @@
 const prisma = require("../lib/prisma");
 
 const findAll = async (params) => {
-    const user_id = +params;
-    const addresses = await prisma.addresses.findMany({
-        where: {
-            user_id: user_id
-        }
-    })
+  const user_id = +params;
+  const addresses = await prisma.addresses.findMany({
+    where: {
+      user_id: user_id,
+    },
+  });
 
-    return addresses;
-}
+  return addresses;
+};
 
 const findOne = async (params) => {
-    const {id, user_id} = params
-    const address = await prisma.addresses.findUnique({
-        where: {
-            id: +id,
-            user_id
-        }
-    })
+  const { id, user_id } = params;
+  const address = await prisma.addresses.findUnique({
+    where: {
+      id: +id,
+      user_id,
+    },
+  });
 
-    if(!address){
-        throw {
-            name: "ErrorNotFound",
-            message: "Data Not Found"
-        }
-    }
+  if (!address) {
+    throw {
+      name: "ErrorNotFound",
+      message: "Data Not Found",
+    };
+  }
 
-    return address;
-}
+  return address;
+};
 
-const create = async ({user_id, params}) => {
-    const city = await prisma.cities.findUnique({
-        where: {
-            id: +params.city_id
-        }
-    })
+const create = async ({ user_id, params }) => {
+  const city = await prisma.cities.findUnique({
+    where: {
+      id: +params.city_id,
+    },
+  });
 
-    if(!city){
-        throw {
-            name: "ErrorNotFound",
-            message: "City Not Found"
-        }
-    }
-    const address = await prisma.addresses.create({
-        data: {
-            user_id: user_id,
-            city_id: params.city_id,
-            street_address: params.street_address,
-            province: params.province,
-            postal_code: params.postal_code
-        }
-    });
+  if (!city) {
+    throw {
+      name: "ErrorNotFound",
+      message: "City Not Found",
+    };
+  }
+  const address = await prisma.addresses.create({
+    data: {
+      user_id: user_id,
+      city_id: params.city_id,
+      street_address: params.street_address,
+      province: params.province,
+      postal_code: params.postal_code,
+      title: params.title,
+    },
+  });
 
-    return address;
-}
+  return address;
+};
 
 const update = async (params) => {
-    const id = await prisma.addresses.findUnique({
-        where: {
-            id: +params.id
-        }
-    });
+  const id = await prisma.addresses.findUnique({
+    where: {
+      id: +params.id,
+    },
+  });
 
-    if (!id) {
-        throw {
-            name: "ErrorNotFound",
-            message: "Data Not Found"
-        }
-    }
+  if (!id) {
+    throw {
+      name: "ErrorNotFound",
+      message: "Data Not Found",
+    };
+  }
 
-    const address = await prisma.addresses.update({
-        where: { id: +params.id },
-        data: {
-            city_id: params.data.city_id,
-            street_address: params.data.street_address,
-            province: params.data.province,
-            postal_code: params.data.postal_code
-        }
-    });
+  const address = await prisma.addresses.update({
+    where: { id: +params.id },
+    data: {
+      city_id: params.data.city_id,
+      street_address: params.data.street_address,
+      province: params.data.province,
+      postal_code: params.data.postal_code,
+      title: params.data.title,
+    },
+  });
 
-    return address;
-}
+  return address;
+};
 
 const destroy = async (params) => {
-    const data = await prisma.addresses.findUnique({
-        where: {
-            id: +params
-        }
-    });
+  const data = await prisma.addresses.findUnique({
+    where: {
+      id: +params,
+    },
+  });
 
-    if (!data) {
-        throw {
-            name: "ErrorNotFound",
-            message: "Data Not Found"
-        }
-    }
-    
-    const address = await prisma.addresses.delete({
-        where: {
-            id: parseInt(params)
-        }
-    });
+  if (!data) {
+    throw {
+      name: "ErrorNotFound",
+      message: "Data Not Found",
+    };
+  }
 
-    return address;
-}
+  const address = await prisma.addresses.delete({
+    where: {
+      id: parseInt(params),
+    },
+  });
 
+  return address;
+};
 
-module.exports = { findAll, findOne, create, update, destroy }
+module.exports = { findAll, findOne, create, update, destroy };
