5ab92c14dff0b04ebd1d6e69adc8488444a08be9
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-05-21 12:25:21 +0800

    Order service update (#17)
    * install jest,supertest, edit wishlist controllers service route bug
    
    * update find all include user fullname & email

--- services/OrderService.js
+++ services/OrderService.js
@@ -86,6 +86,12 @@ const findAll = async (params) => {
             product: true,
           },
         },
+        user: {
+          select: {
+            fullname: true,
+            email: true,
+          },
+        },
       },
     }),
     prisma.orders.count({
